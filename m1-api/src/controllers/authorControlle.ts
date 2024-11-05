eae80bc8968d853e7c4ffc2e927e69154f70185c
HugoVitse <hugovitse1@gmail.com>
2024-11-05 09:25:34 +0100

    adding controllers

---
+++ m1-api/src/controllers/authorControlle.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { AppService } from '../app.service';
+
+@Controller('authors')
+export class authorController {
+  constructor(private readonly authorService: AppService) {}
+
+  @Get('/authors')
+  public async getAllauthors() : Promise<string> {
+    return this.authorService.getHello();
+  }
+
+  @Get('/authors/:id')
+  public async getauthor(@Param('id') id: string) : Promise<string> {
+    return this.authorService.getHello();
+  }
+}
